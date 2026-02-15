f94d52bdc084b6d7eb7dee8313049de3729e3351
AdrianMroczek <adrian.mroczek96@gmail.com>
2026-02-15 18:30:32 +0100

    Add catalog page checks and update Jenkinsfile for test execution

--- tests/pages/catalog_page.py
+++ tests/pages/catalog_page.py
@@ -40,3 +40,35 @@ class CatalogPage:
         row = self.page.locator("tr").filter(has_text=product_name)
         row.locator("input[type='number']").fill(str(quantity))
         self.get_order_button_by_name(product_name).click()
+
+
+### Check methods ###
+
+    def check_order_button_status(self, product_name: str, expected_status: str):
+        button = self.get_order_button_by_name(product_name)
+        if expected_status == "enabled":
+            expect(button).not_to_be_disabled()
+        elif expected_status == "disabled":
+            expect(button).to_be_disabled()
+        else:
+            raise ValueError("Invalid expected status. Use 'enabled' or 'disabled'.")
+
+    def check_availability(self, product_name: str, expected_availability: str):
+        row = self.page.locator("tr").filter(has_text=product_name)
+        availability_cell = row.locator("td").nth(3).locator("span")
+        if expected_availability == "not available":
+            expect(availability_cell).to_have_class("badge bg-danger")
+        elif expected_availability == "available":
+            expect(availability_cell).to_have_class("badge bg-success")
+        else:
+            raise ValueError("Invalid expected availability. Use 'available' or 'not available'.")
+
+    def check_amount(self, product_name: str, expected_amount: str):
+        row = self.page.locator("tr").filter(has_text=product_name)
+        amount_cell = row.locator("td").nth(3).locator("span")
+        expect(amount_cell).to_contain_text(expected_amount)
+
+    def check_price(self, product_name: str, expected_price: str):
+        row = self.page.locator("tr").filter(has_text=product_name)
+        price_cell = row.locator("td").nth(2)
+        expect(price_cell).to_contain_text(expected_price)
