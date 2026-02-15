0f4c626002058938a5481a151ff3369105f18f25
AdrianMroczek <adrian.mroczek96@gmail.com>
2026-02-15 16:19:00 +0100

    Add preparation stage to Jenkinsfile for Python dependencies and Playwright installation

--- tests/sanity_tests/test_UI_sanity.py
+++ tests/sanity_tests/test_UI_sanity.py
@@ -9,4 +9,21 @@ def test_filter_by_max_price(page):
     catalog.apply_filter()
     catalog.products_list(["Logitech Lift"])
 
+def test_filter_by_category(page):
+    catalog = CatalogPage(page)
+    page.goto("http://127.0.0.1:8000/")
+    expect(page).to_have_title("Warehouse System - Widok Klienta")
+    catalog.select_category("Laptopy")
+    catalog.apply_filter()
+    catalog.products_list(["ASUS ROG Strix G16"])
+
+def test_double_filter(page):
+    catalog = CatalogPage(page)
+    page.goto("http://127.0.0.1:8000/")
+    expect(page).to_have_title("Warehouse System - Widok Klienta")
+    catalog.set_name_filter("Lenovo")
+    catalog.select_category("Monitory")
+    catalog.apply_filter()
+    catalog.products_list(["Lenovo Legion 27Q-10"])
+
 
